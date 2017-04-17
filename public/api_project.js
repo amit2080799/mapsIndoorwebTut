ee1c462b4a69e01328a17635ebcc827a3fb609ad
Amine <Amine@Amine.local>
2017-04-17 17:42:18 +0100

    new updates

--- public/api_project.js
+++ public/api_project.js
@@ -1,1 +1,1 @@
-define({  "name": "tunsie-sanitaire",  "version": "0.0.0",  "description": "",  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2017-04-16T22:09:40.848Z",    "url": "http://apidocjs.com",    "version": "0.16.1"  }});
+define({  "name": "tunsie-sanitaire",  "version": "0.0.0",  "description": "",  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2017-04-17T16:39:10.492Z",    "url": "http://apidocjs.com",    "version": "0.16.1"  }});
